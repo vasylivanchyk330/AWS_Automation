a0b5e88ae0c0a8171a44bf1a51739db84adc5bb7
Ivanchyk <vasyl.ivanchyk@accenture.com>
2024-06-05 12:51:28 +0200

    added basic functionality for handling delete markers

--- s3/bucket-cleanup/bucket-content-cleanup/stable--delete-s3-bucket-content.py
+++ s3/bucket-cleanup/bucket-content-cleanup/stable--delete-s3-bucket-content.py
@@ -21,6 +21,20 @@ def delete_objects_in_page(bucket_name, objects):
         s3.delete_objects(Bucket=bucket_name, Delete={'Objects': objects})
         print(f"Deleted {len(objects)} objects from bucket '{bucket_name}'.")
 
+def delete_delete_markers(bucket_name):
+    s3 = boto3.client('s3')
+    paginator = s3.get_paginator('list_object_versions')
+
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for page in paginator.paginate(Bucket=bucket_name):
+            if 'DeleteMarkers' in page:
+                delete_markers = [{'Key': marker['Key'], 'VersionId': marker['VersionId']} for marker in page['DeleteMarkers']]
+                futures.append(executor.submit(delete_objects_in_page, bucket_name, delete_markers))
+        
+        for future in futures:
+            future.result()
+
 def delete_objects(bucket_name):
     s3 = boto3.client('s3')
     paginator = s3.get_paginator('list_objects_v2')
@@ -45,9 +59,9 @@ if __name__ == "__main__":
     for bucket_name in bucket_names:
         if bucket_exists(bucket_name):
             delete_objects(bucket_name)
+            delete_delete_markers(bucket_name)
         else:
             print(f"Bucket '{bucket_name}' does not exist.")
-
 
 # ###########
 end_time = time.time()
