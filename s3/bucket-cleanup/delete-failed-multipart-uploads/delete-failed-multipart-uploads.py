67cd33ff82d2814454526bdf6e6fa4b5fbed2a1f
Ivanchyk <vasyl.ivanchyk@accenture.com>
2024-06-06 09:27:48 +0200

    added min version

---
+++ s3/bucket-cleanup/delete-failed-multipart-uploads/delete-failed-multipart-uploads.py
@@ -0,0 +1,31 @@
+import sys
+import boto3
+
+def delete_failed_multipart_uploads(bucket_name):
+    s3_client = boto3.client('s3')
+    try:
+        multipart_uploads = s3_client.list_multipart_uploads(Bucket=bucket_name)
+        if 'Uploads' in multipart_uploads:
+            for upload in multipart_uploads['Uploads']:
+                s3_client.abort_multipart_upload(
+                    Bucket=bucket_name,
+                    Key=upload['Key'],
+                    UploadId=upload['UploadId']
+                )
+                print(f"Aborted multipart upload: {upload['Key']} with UploadId: {upload['UploadId']}")
+        else:
+            print(f"No failed multipart uploads found for bucket: {bucket_name}")
+    except Exception as e:
+        print(f"Failed to check or delete multipart uploads for {bucket_name}: {e}")
+        sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <bucket-name>")
+        sys.exit(1)
+
+    bucket_name = sys.argv[1]
+    delete_failed_multipart_uploads(bucket_name)
+
+if __name__ == "__main__":
+    main()
